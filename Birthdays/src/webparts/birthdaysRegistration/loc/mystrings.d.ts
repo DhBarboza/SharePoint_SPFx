6e2fe35e6a9c857ea18b8aa655b3b0e0bbb912b4
Douglas Henrique <dev.dhbarboza@gmail.com>
2023-12-19 23:13:30 -0300

    init project birthdays

---
+++ Birthdays/src/webparts/birthdaysRegistration/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IBirthdaysRegistrationWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'BirthdaysRegistrationWebPartStrings' {
+  const strings: IBirthdaysRegistrationWebPartStrings;
+  export = strings;
+}
---
+++ Birthdays/src/webparts/listBirthdays/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IListBirthdaysWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ListBirthdaysWebPartStrings' {
+  const strings: IListBirthdaysWebPartStrings;
+  export = strings;
+}
