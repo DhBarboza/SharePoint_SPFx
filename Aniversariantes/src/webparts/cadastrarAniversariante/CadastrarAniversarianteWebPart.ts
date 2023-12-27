8d15cfe0a3f55b1ce1944cbc4b4267abb9a2c16f
Douglas Henrique <dev.dhbarboza@gmail.com>
2023-12-26 23:03:42 -0300

    Incluido as dependencias e métodos

--- Aniversariantes/src/webparts/cadastrarAniversariante/CadastrarAniversarianteWebPart.ts
+++ Aniversariantes/src/webparts/cadastrarAniversariante/CadastrarAniversarianteWebPart.ts
@@ -1,61 +1,144 @@
-import { Version } from '@microsoft/sp-core-library';
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { escape } from '@microsoft/sp-lodash-subset';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-import styles from './CadastrarAniversarianteWebPart.module.scss';
-import * as strings from 'CadastrarAniversarianteWebPartStrings';
+//import styles from './CadastrarAniversarianteWebPart.module.scss';
+import * as strings from "CadastrarAniversarianteWebPartStrings";
+
+import { sp, ItemAddResult } from "@pnp/sp";
+import * as $ from "jquery";
+import "bootstrap";
+
+require("../../../node_modules/bootstrap/dist/css/bootstrap.min.css");
+require("../../../node_modules/@fortawesome/fontawesome-free/css/all.min.css");
+
+//css padrao
+require("../../stylelibrary/css/padrao.css");
+
+//importa toast
+require("../../stylelibrary/css/toastr.min.css");
+import toastr from "../../stylelibrary/js/toast/toastr.min.js";
 
 export interface ICadastrarAniversarianteWebPartProps {
   description: string;
 }
 
-export default class CadastrarAniversarianteWebPart extends BaseClientSideWebPart <ICadastrarAniversarianteWebPartProps> {
-
-  public render(): void {
-    this.domElement.innerHTML = `
-      <div class="${ styles.cadastrarAniversariante }">
-    <div class="${ styles.container }">
-      <div class="${ styles.row }">
-        <div class="${ styles.column }">
-          <span class="${ styles.title }">Welcome to SharePoint!</span>
-  <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-    <p class="${ styles.description }">${escape(this.properties.description)}</p>
-      <a href="https://aka.ms/spfx" class="${ styles.button }">
-        <span class="${ styles.label }">Learn more</span>
-          </a>
-          </div>
-          </div>
-          </div>
-          </div>`;
+export default class CadastrarAniversarianteWebPart extends BaseClientSideWebPart<ICadastrarAniversarianteWebPartProps> {
+  public onInit(): Promise<void> {
+    return super.onInit().then((_) => {
+      sp.setup({
+        spfxContext: this.context,
+      });
+    });
   }
 
-  protected get dataVersion(): Version {
-  return Version.parse('1.0');
-}
+  public render(): void {
+    //carrego o template de layout
+    this.domElement.innerHTML = require("./template.html");
+
+    document
+      .getElementById("btnSalvar")
+      .addEventListener("click", () => this.SalvarAniversariante());
+
+    this.ObterTodasAreas();
+  }
+
+  public async ObterTodasAreas() {
+    let montahtmlAreas;
+
+    await sp.web.lists
+      .getByTitle("Area")
+      .items.top(5000)
+      .select("ID, Title")
+      .orderBy("Title", true)
+      .get()
+      .then((items) => {
+        montahtmlAreas = `<option value="">Selecione</option>`;
+        items.forEach((element) => {
+          montahtmlAreas += `<option value="${element.Title}">${element.Title}</option>`;
+        });
+
+        $("#ddlArea").html(montahtmlAreas);
+      })
+      .catch((e) => {
+        console.log("erro", e);
+      });
+  }
+
+  protected SalvarAniversariante(): void {
+    toastr.options = {
+      closeButton: false,
+      debug: false,
+      newestOnTop: false,
+      progressBar: true,
+      positionClass: "toast-top-center",
+      preventDuplicates: false,
+      onclick: null,
+      showDuration: "300",
+      hideDuration: "1000",
+      timeOut: "4000",
+      extendedTimeOut: "1000",
+      showEasing: "swing",
+      hideEasing: "linear",
+      showMethod: "fadeIn",
+      hideMethod: "fadeOut",
+    };
+
+    let Nome = $("#txtNome").val();
+    let DataAniversario = new Date(
+      `${$("#txtDataAniversario").val()}T12:00:00`
+    ).toISOString();
+    let Area = $("#ddlArea option:selected").text();
+    let UrlFoto = $("#txtURLFoto").val();
+    let Observacao = $("#txtObservacao").val();
+
+    sp.web.lists
+      .getByTitle("Birthdays")
+      .items.add({
+        Title: Nome,
+        BirthdayDate: DataAniversario,
+        Area: Area,
+        UrlPhoto: UrlFoto,
+        Observation: Observacao,
+      })
+      .then(
+        (iar: ItemAddResult) => {
+          toastr["success"]("Cadastro realizado com sucesso!", "Sucesso");
+        },
+        (err) => {
+          console.log(err);
+          toastr["error"]("Ocorreu um erro ao realizar o cadastro.", "Erro");
+        }
+      );
+  }
+
+  /*protected get dataVersion(): Version {
+    return Version.parse("1.0");
+  }*/
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-  return {
-    pages: [
-      {
-        header: {
-          description: strings.PropertyPaneDescription
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription,
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
         },
-        groups: [
-          {
-            groupName: strings.BasicGroupName,
-            groupFields: [
-              PropertyPaneTextField('description', {
-                label: strings.DescriptionFieldLabel
-              })
-            ]
-          }
-        ]
-      }
-    ]
-  };
-}
+      ],
+    };
+  }
 }
